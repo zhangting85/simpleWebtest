759928d19ba808a17805a8c2a7b71bd5295729cc
Ting <89507288@qq.com>
2014-03-30 11:00:35 +0800

    create the project with maven

---
+++ src/main/java/simplewebtest/App.java
@@ -0,0 +1,13 @@
+package simplewebtest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/simplewebtest/test/TestWebdriverEnv.java
@@ -0,0 +1,24 @@
+package simplewebtest.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class TestWebdriverEnv {
+
+	@Test
+	public void checkEnv(){
+		//首先打一个hello world来测试一你的IDE里testng的插件是否已经安装好
+		//check if you success installed testng on your IDE
+		System.out.println("Hello World, TestNG");
+		
+		//看看你电脑上能不能正确用firefox driver启动firefox
+		//check if firefox driver runs successfully on your computer
+		WebDriver driver=new FirefoxDriver();
+		driver.get("https://github.com/zhangting85/simpleWebtest");
+	
+	}
+	
+	
+	
+}
